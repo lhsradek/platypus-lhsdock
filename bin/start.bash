1c267d503babf06b99d94d78ece55727b5adb1d9
Radek Kadner <radek.kadner@gmail.com>
2022-08-06 16:21:27 +0200

    .

--- bin/start.bash
+++ bin/start.bash
@@ -2,18 +2,18 @@
 
 echo == lhsdock start ==
 
-mkdir -p ~/lhsdock/certs
-mkdir -p ~/lhsdock/content
-mkdir -p ~/lhsdock/content/projekt1
-mkdir -p ~/lhsdock/content/projekt1/lhsdock
-cp -ur context/html/* ~/lhsdock/content
-cp -u readme.md ~/lhsdock/content/projekt1/lhsdock/readme.txt
-cp -u context/root/bin/READme.html ~/lhsdock/content/projekt1/lhsdock
-cp -u dockerfiles/Dockerfile ~/lhsdock/content/projekt1/lhsdock/Dockerfile.txt
-cp -u lhsdock:v3.img ~/lhsdock/content/projekt1/lhsdock/
-chmod a+r ~/lhsdock/content/projekt1/lhsdock/lhsdock:v3.img
-chmod a-w ~/lhsdock/content
 docker start platypus-lhsdock
 bin/ls.bash | grep lhsdock
+mkdir -p ~/volume/lhsdock/certs
+mkdir -p ~/volume/lhsdock/content
+mkdir -p ~/volume/lhsdock/content/projekt1
+mkdir -p ~/volume/lhsdock/content/projekt1/lhsdock
+cp -ur context/html/* ~/volume/lhsdock/content
+cp -u readme.md ~/volume/lhsdock/content/projekt1/lhsdock/readme.txt
+cp -u context/root/bin/READme.html ~/volume/lhsdock/content/projekt1/lhsdock
+cp -u dockerfiles/Dockerfile ~/volume/lhsdock/content/projekt1/lhsdock/Dockerfile.txt
+cp -u lhsdock:v3.img ~/volume/lhsdock/content/projekt1/lhsdock/
+chmod a+r ~/volume/lhsdock/content/projekt1/lhsdock/lhsdock:v3.img
+chmod a-w ~/volume/lhsdock/content
 echo Run terminal with:
 echo "./bin/exec.bash"
