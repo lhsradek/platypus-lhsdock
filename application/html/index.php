4d9d9b1455db97fdc625bebb34c33484e3547d74
Radek Kadner <radek.kadner@gmail.com>
2022-08-12 18:53:26 +0200

    .

--- application/html/index.php
+++ application/html/index.php
@@ -78,8 +78,8 @@
             <ul>
 	      <li><a href="https://<?php print($parent)?>.nginx.local"><?php print($parent)?>.nginx.local</a></li>
               <li><a href="https://<?php print($parent)?>.phpmyadmin.local"><?php print($parent)?>.phpmyadmin.local</a></li>
-              <li><a href="https://<?php print($parent)?>.wordpress.local"><?php print($parent)?>.wordpress.local</a></li>
               <li><a href="https://<?php print($parent)?>.tomcat.local"><?php print($parent)?>.tomcat.local</a></li>
+              <li><a href="https://<?php print($parent)?>.wordpress.local"><?php print($parent)?>.wordpress.local</a></li>
             </ul>
           </ul>
         </p>
