47a8270714888c4c310613621bd85458f1baf134
Radek Kadner <radek.kadner@gmail.com>
2022-08-13 19:27:12 +0200

    .

--- application/html/index.php
+++ application/html/index.php
@@ -89,7 +89,7 @@
               <li><a href="https://<?php print($parent)?>.wordpress.local"><?php print($parent)?>.wordpress.local</a></li>
               <li><a href="https://pma.<?php print($parent)?>.wordpress.local">pma.<?php print($parent)?>.wordpress.local</a></li>
               <li><a href="https://<?php print($parent)?>.tomcat.local"><?php print($parent)?>.tomcat.local</a></li>
-              <li><a href="https://<?php print($parent)?>.phpmyadmin.local"><?php print($parent)?>.phpmyadmin.local</a></li>
+              <!-- <li><a href="https://<?php print($parent)?>.phpmyadmin.local"><?php print($parent)?>.phpmyadmin.local</a></li> -->
             </ul>
           </ul>
         </p>
