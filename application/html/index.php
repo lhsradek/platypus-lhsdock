8cbc99b137e7b1cbde56ec9888d3269df12308f8
Radek Kadner <radek.kadner@gmail.com>
2022-08-28 16:12:20 +0200

    .

--- application/html/index.php
+++ application/html/index.php
@@ -156,6 +156,7 @@ if ($printEnv) {
         <h2>Documentation</h2>
         <p>
           <ul>
+              <li><a href="https://hub.docker.com/_/adminer" target="_blank">hub.docker.com - Official build of Adminer</a></li>
               <li><a href="https://hub.docker.com/_/alpine" target="_blank">hub.docker.com - Official build of Alpine Linux</a></li>
               <li><a href="https://hub.docker.com/_/mariadb" target="_blank">hub.docker.com - Official build of MariaDB</a></li>
               <li><a href="https://hub.docker.com/_/nginx" target="_blank">hub.docker.com - Official build of Nginx</a></li>
