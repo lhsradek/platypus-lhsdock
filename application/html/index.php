c8662b5ae7c930cf86c310e36829a97a3838f12c
Radek Kádner <radek.kadner@gmail.com>
2022-12-08 15:25:48 +0100

    .

--- application/html/index.php
+++ application/html/index.php
@@ -132,6 +132,7 @@ if ($parent == "docker") {
   $isTraefik = @$index->isUrl("https://".$parent.".traefik.local");
 }
 $isKibana = @$index->isUrl("https://kibana.".$host.".".$net.":5601");
+$isKibanaPlatel = @$index->isUrl("https://kibana.pt.platel:5601");
 $isCerebro = @$index->isSocket("cerebro.".$parent.".".$net, 9000);
 $isEs01 = @$index->isUrl("https://es01.".$host.".".$net.":9200");
 $isEs02 = false; // @$index->isUrl("https://es02.".$host.".".$net.":9201");
@@ -201,6 +202,9 @@ if ($printEnv) {
 		} if($isWiki) {
 			$index->printWiki("kibana");
 		} ?></li><?php
+	}
+	if($isKibanaPlatel) { ?><li>
+<a href="https://kibana.pt.platel:5601" target="_blank">kibana.pt.platel</a> <?php
 	}
 	if($isCerebro|| $isWiki) { ?><li><?php
 		if($isCerebro) { ?>
