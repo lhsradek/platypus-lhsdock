8ed5d3c906a30ff98fa58e062d5c9216577ce10d
Radek Kádner <radek.kadner@gmail.com>
2022-12-26 04:06:18 +0100

    .

---
+++ extras/filebeat/filebeat/install.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+# docker exec -it lhsdock-filebeat bash
+# ~/bin/install.bash 
+# exit
+# docker stop lhsdoc-filebeat
+# docker start lhsdock-filebeat
+
+rm -f /usr/share/filebeat/modules.d/elasticsearch.yml
+rm -f /usr/share/filebeat/modules.d/elasticsearch.yml.disabled
+cp /root/bin/elasticsearch.yml /usr/share/filebeat/modules.d/elasticsearch.yml
+rm -f /usr/share/filebeat/modules.d/kibana.yml
+rm -f /usr/share/filebeat/modules.d/kibana.yml.disabled
+cp /root/bin/kibana.yml /usr/share/filebeat/modules.d/kibana.yml
+rm -f /usr/share/filebeat/modules.d/traefik.yml
+rm -f /usr/share/filebeat/modules.d/traefik.yml.disabled
+cp /root/bin/traefik.yml /usr/share/filebeat/modules.d/traefik.yml
+rm -f /usr/share/filebeat/modules.d/nginx.yml
+rm -f /usr/share/filebeat/modules.d/nginx.yml.disabled
+cp /root/bin/nginx.yml /usr/share/filebeat/modules.d/nginx.yml
+chown root.root /usr/share/filebeat/modules.d/*
---
+++ extras/metricbeat/metricbeat/install.bash
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+# docker exec -it lhsdock-metricbeat bash
+# ~/bin/install.bash               
+# exit
+# docker stop lhsdock-metricbeat
+# docker start lhsdock-metricbeat
+
+rm -f /usr/share/metricbeat/modules.d/beat-xpack.yml
+rm -f /usr/share/metricbeat/modules.d/beat-xpack.yml.disabled
+cp /root/bin/beat-xpack.yml /usr/share/metricbeat/modules.d/beat-xpack.yml
+rm -f /usr/share/metricbeat/modules.d/elasticsearch-xpack.yml
+rm -f /usr/share/metricbeat/modules.d/elasticsearch-xpack.yml.disabled
+cp /root/bin/elasticsearch-xpack.yml /usr/share/metricbeat/modules.d/elasticsearch-xpack.yml
+rm -f /usr/share/metricbeat/modules.d/kibana-xpack.yml
+rm -f /usr/share/metricbeat/modules.d/kibana-xpack.yml.disabled
+cp /root/bin/kibana-xpack.yml /usr/share/metricbeat/modules.d/kibana-xpack.yml
+rm -f /usr/share/metricbeat/modules.d/logstash-xpack.yml
+rm -f /usr/share/metricbeat/modules.d/logstash-xpack.yml.disabled
+cp /root/bin/logstash-xpack.yml /usr/share/metricbeat/modules.d/logstash-xpack.yml
+rm -f /usr/share/metricbeat/modules.d/nginx.yml
+rm -f /usr/share/metricbeat/modules.d/nginx.yml.disabled
+cp /root/bin/nginx.yml /usr/share/metricbeat/modules.d/nginx.yml
+rm -f /usr/share/metricbeat/modules.d/php_fpm.yml
+rm -f /usr/share/metricbeat/modules.d/php_fpm.yml.disabled
+cp /root/bin/nginx.yml /usr/share/metricbeat/modules.d/php_fpm.yml
+rm -f /usr/share/metricbeat/modules.d/system.yml
+rm -f /usr/share/metricbeat/modules.d/system.yml.disabled
+cp /root/bin/system.yml /usr/share/metricbeat/modules.d/system.yml
+rm -f /usr/share/metricbeat/modules.d/docker.yml
+rm -f /usr/share/metricbeat/modules.d/docker.yml.disabled
+cp /root/bin/docker.yml /usr/share/metricbeat/modules.d/docker.yml
+chown root.root /usr/share/metricbeat/modules.d/*
